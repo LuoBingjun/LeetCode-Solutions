ca1196bc53b6ca09a43eb8eda9be27b2570df53e
Bingjun Luo <luobingjun@gmail.com>
2020-07-01 02:26:03 +0800

    p4-2: 使用二分查找将时间降到O(log(m+n)) 关键在于找到数组指针i,j的计算方法，熟练使用整除统一奇偶两种情况

--- 4.median-of-two-sorted-arrays.py
+++ 4.median-of-two-sorted-arrays.py
@@ -4,35 +4,46 @@
 # [4] Median of Two Sorted Arrays
 #
 
+from typing import List
+
 # @lc code=start
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums2) < len(nums1):
+            return self.findMedianSortedArrays(nums2, nums1)
+
         m = len(nums1)
         n = len(nums2)
-        l = list()
-        i = j = last = 0
-        while True:
-            if i == m and j == n:
-                break
-            if i == m:
-                l.append(nums2[j])
-                j = j + 1
-            elif j == n:
-                l.append(nums1[i])
-                i = i + 1
-            else:
-                if nums1[i] < nums2[j]:
-                    l.append(nums1[i])
-                    i = i + 1
-                else:
-                    l.append(nums2[j])
-                    j = j + 1
 
-        length = len(l)
-        if length % 2 == 0:
-            return (l[length // 2 - 1] + l[length // 2]) / 2
-        else:
-            return l[length // 2]
+        l = 0
+        h = m
+
+        while l <= h:
+            i = l + (h - l) // 2
+            j = (m + n + 1) // 2 - i
+
+            l1 = nums1[i - 1] if i - 1 >= 0 else float("-inf")
+            r1 = nums1[i] if i < m else float("inf")
+            l2 = nums2[j - 1] if j - 1 >= 0 else float("-inf")
+            r2 = nums2[j] if j < n else float("inf")
+            
+            if l1 <= r2 and l2 <= r1:
+                if (m + n) % 2 == 0:
+                    return (max(l1, l2) + min(r1, r2)) / 2
+                else:
+                    return max(l1, l2)
+            elif l1 > r2:
+                h = i - 1
+            elif l2 > r1:
+                l = i + 1
 
 # @lc code=end
 
+sol = Solution()
+print(sol.findMedianSortedArrays([1, 3],[2]))
+print(sol.findMedianSortedArrays([1, 2],[3, 4]))
+print(sol.findMedianSortedArrays([1, 3],[2, 4]))
+print(sol.findMedianSortedArrays([],[1]))
+print(sol.findMedianSortedArrays([1],[]))
+print(sol.findMedianSortedArrays([],[2, 3]))
+print(sol.findMedianSortedArrays([3], [-2,-1]))
