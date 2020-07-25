b0d87a45a45d08182459940dc1c6831e4e018e01
Bingjun Luo <luobingjun@gmail.com>
2020-07-25 11:22:12 +0800

    p84-2: 使用left和right数组记录包含当前柱子的最大矩形的左端和右端，遍历计算 left[i]为i以左比i低且离i最近的柱子，right[i]同理

--- 84.largest-rectangle-in-histogram.cpp
+++ 84.largest-rectangle-in-histogram.cpp
@@ -12,34 +12,32 @@ class Solution
 public:
     int largestRectangleArea(vector<int> &heights)
     {
-        if (heights.empty())
+        int n = heights.size();
+        if (n == 0)
             return 0;
-        int max_rect = 0;
-        stack<int> st;
-        int i = 0;
-        while (i < heights.size())
+        vector<int> left(n);
+        vector<int> right(n);
+        left[0] = -1;
+        right[n - 1] = n;
+        for (int i = 1; i < n; i++)
         {
-            while (!st.empty() && heights[st.top()] > heights[i])
-            {
-                int h = heights[st.top()];
-                st.pop();
-                if (st.empty())
-                    max_rect = max(max_rect, i * h);
-                else
-                    max_rect = max(max_rect, (i - st.top() - 1) * h);
-            }
-            st.push(i);
-            i++;
+            int j = i - 1;
+            while (j >= 0 && heights[j] >= heights[i])
+                j = left[j];
+            left[i] = j;
+        }
+        for (int i = n - 2; i >= 0; i--)
+        {
+            int j = i + 1;
+            while (j < n && heights[j] >= heights[i])
+                j = right[j];
+            right[i] = j;
         }
 
-        while (!st.empty())
+        int max_rect = 0;
+        for (int i = 0; i < n; i++)
         {
-            int h = heights[st.top()];
-            st.pop();
-            if (st.empty())
-                max_rect = max(max_rect, i * h);
-            else
-                max_rect = max(max_rect, (i - st.top() - 1) * h);
+            max_rect = max(max_rect, heights[i] * (right[i] - left[i] - 1));
         }
         return max_rect;
     }
@@ -49,7 +47,9 @@ public:
 int main()
 {
     Solution sol;
-    vector<int> input({2, 1, 5, 6, 2, 3});
-    sol.largestRectangleArea(input);
+    vector<int> input1({2, 1, 5, 6, 2, 3});
+    sol.largestRectangleArea(input1);
+    vector<int> input2({1, 1});
+    sol.largestRectangleArea(input2);
     return 0;
 }
